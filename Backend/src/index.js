d84f31af09522ce7cc4f64988e5b6411287bced4
shashikant-bhardwaj <bhardwajshashi531@gmail.com>
2026-08-15 22:24:02 +0530

    add database connection setup and constants

---
+++ Backend/src/index.js
@@ -0,0 +1,26 @@
+import express from "express"
+import dotenv from "dotenv"
+import { connectDB } from "./db/index.js";
+
+dotenv.config({
+    path: "./.env"
+})
+
+const app = express();
+
+connectDB()
+.then( () => {
+    app.on("error", (error) => {
+        console.log(`HTTP server error`, error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`server is runing on port: ${process.env.PORT}`);
+    })
+})
+.catch( (error) => {
+    console.log(`MONGODB connection failed !!!`, error);
+    throw error
+})
+
+export { app }
